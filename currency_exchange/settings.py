ee25ba84df5bbb83cd914484b78d0fb956c1412f
mhykeborhlar <michealadefehinti09@gmail.com>
2025-10-28 14:58:04 +0000

    changed some file

--- currency_exchange/settings.py
+++ currency_exchange/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(^0rz#$u#@oby+)33*8t^y9%i3!5s(@ure5t4a*owze@)ju6t6'
+SECRET_KEY = 'django-insecure-(^0rz
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
