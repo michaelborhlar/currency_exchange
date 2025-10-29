870b93976fee8aa91afe66aa4321f32fb6413355
mhykeborhlar <michealadefehinti09@gmail.com>
2025-10-29 12:46:54 +0000

    changed some files

--- countries/urls.py
+++ countries/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path("countries/refresh", RefreshCountriesView.as_view(), name="refresh_countries"),
     path("countries/image", SummaryImageView.as_view(), name="country_image"),
-    path("countries", CountryListView.as_view(), name="list_countries"),
     path("countries/<str:name>", CountryDetailView.as_view(), name="country_detail"),
+    path("countries", CountryListView.as_view(), name="list_countries"),
     path("status", StatusView.as_view(), name="status"),
 ]
